ebecdd66252e1228a1271757f0a7799adf04136f
indisersa <indisersa@radissonguat.com>
2017-10-23 12:51:10 -0600

    message

--- scrapers/processors.py
+++ scrapers/processors.py
@@ -42,22 +42,29 @@ class scroll_down(object):
         
         raise ValueError('error')
 
+class spider(object):
+    @staticmethod
+    def chrome(url):
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get(url)
+        return driver
+
+    @staticmethod
+    def chrome_noImages(url):
+        chrome_options = webdriver.ChromeOptions()
+        prefs = {"profile.managed_default_content_settings.images": 2}
+        chrome_options.add_experimental_option("prefs", prefs)
+        driver = webdriver.Chrome(chrome_options=chrome_options)
+        driver.get(url)
+        return driver
+
 def sql_write(conn, cur, hotel, rating, review, address, new_price, old_price, checkin, checkout, city, currency, source, count, date):
     hotel = hotel.replace("'", "''")
     address = address.replace("'", "''")
     city = city.replace("'", "''")
     sql = "insert into hotel_info (hotel_name, hotel_rating, hotel_review, hotel_address, new_price, old_price, checkin, checkout, city, currency, source, date_scraped, hotel_position, date_range) values('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', %s)" % (hotel, rating, review, address, new_price, old_price, checkin, checkout, city, currency, source, datetime.now().strftime('%m/%d/%Y'), count, date)
     sql_exec(conn, cur, sql)
-
-def spider(url):
-    chrome_options = webdriver.ChromeOptions()
-    prefs = {"profile.managed_default_content_settings.images":2}
-    chrome_options.add_experimental_option("prefs",prefs)
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-    #driver.set_window_size(800, 600)
-    driver.get(url)
-    time.sleep(5)
-    return driver
 
 def csv_write(fh, name, review, rating, address, currency, new_price, old_price, checkin, checkout, city, source):
         line = '"%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s"\n' % (name, review, rating, address, currency, new_price, old_price, checkin, checkout, city, source)
